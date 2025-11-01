0dc5103efc43afd2852eff72d7d4f02f9b40890e
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2025-11-01 22:09:38 +0530

    js practice loops

--- Day1/script.js
+++ Day1/script.js
@@ -2,5 +2,18 @@
 // var b=a;
 // b.pop();
 
-let a= prompt("name");
-alert (`hey ${a}`);
+// let a= prompt("name");
+// alert (`hey ${a}`);
+
+// let username =prompt("username batao");
+// console.log( typeof username);
+
+//  let age= prompt("age batao");
+// Number(age)
+
+// let msg =("i love sheriyans");
+
+// expressions produce a value but statement does't
+
+let z= symbol("id");
+console.log("z");
---
+++ day8/script.js
@@ -0,0 +1,79 @@
+// q-1
+// for(let i=1; i<11;i++ ){
+//     console.log(i);
+// }
+
+// q-2
+// for(let i=1; i<=20;i++){
+//     if(i%2 ===0 ){
+//          console.log(i);
+//     }
+// }
+
+// for(let i=2; i<21;i+=2){
+//     console.log(i);
+// }
+
+// q-3
+// for (let i=10; i>0;i--){
+//     console.log(i);
+// }
+
+// q-4
+// for(let i=0;i<=5;i++){
+//     console.log("yes");
+// }
+
+// q-5
+
+// for(let i=1;i<11;i++)
+// {
+//     if(i%2===0){
+//         console.log(`${i} - even`);
+//     }else console.log(`${i}- odd`);
+// }
+
+// q-6
+
+// let num = +prompt("number batao"); jab bhi prompt se kuch  mangoge to socho kya wo number hai toh use convert karna padega 1) parseInt 2)+prompt
+
+// if (num===12){
+//     console.log("it is 12");
+// } else {
+//     console.log("not");
+// }
+
+// let num = prompt("number batao");
+// num= parseInt(num);
+// console.log(typeof num);
+
+// let num = prompt("number batao");
+// if(num>=0){
+//     console.log("positive")
+// }else {
+//     console.log(negative);
+// }
+
+// q-7
+
+// let age = prompt("what is your age?");
+//  if(age>=18){
+//     console.log("eligible");
+//  }else{
+//     console.log("not eligible");
+//  }
+
+// let age = prompt("what is your age?");
+// if (age===null){
+//     console.log("u pressed cancel");
+// }
+
+let age = prompt("what is your age?");
+
+if (age===null){
+    console.error("null hai bc");
+}else{
+if (age.trim() === ""){
+   console.log(" dhang se likh ");
+}
+}
