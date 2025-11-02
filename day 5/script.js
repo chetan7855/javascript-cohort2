c0388e1bfd706a4d69ca84c3ed0c04eb8fe0c215
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2025-11-02 18:17:50 +0530

    js operations practice questions

--- day 5/script.js
+++ day 5/script.js
@@ -1,1 +1,25 @@
-let arr=[];
+// let arr=[];
+// console.log(b);
+// var b = 12;
+
+// let a = 5;
+// let b= 18;
+// console.log( a * b);
+
+// let x= 5;
+// x += 20;
+// console.log(x);
+
+// let count=5;
+// console.log(count);
+// count++;
+// console.log(count);
+
+// 5 == '5';
+
+let a= prompt("enter the number :");
+if(a>5 && a<20 && a===10){
+    console.log("greater");
+}else{
+    console.log("less then 20");
+}
